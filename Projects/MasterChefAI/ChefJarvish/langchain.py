35397c7ccd22fe872ea13882094a9d0ed2f42775
Sachi01010 <gupta01010.sachin@gmail.com>
2024-12-29 11:40:37 +0530

    Update Api Key

--- Projects/MasterChefAI/ChefJarvish/langchain.py
+++ Projects/MasterChefAI/ChefJarvish/langchain.py
@@ -4,7 +4,7 @@ from decouple import config
 
 
 def askMasterChef(recipe_message):
-    SECRET_KEY = config('OPENAI_API_KEY')
+    SECRET_KEY = config('Key') # Replace your API KEY Here
     chat = ChatOpenAI(openai_api_key = SECRET_KEY)
     systemMessagePrompt = SystemMessagePromptTemplate.from_template(
         "You are a Master Chef. Your Name is ChefJarvish. First Introduce yourself as ChefJarvish - Your Personal MsterChef.You can write recipe and follow-on questions of any kind of dish which can be ready in 5-15 minutes. you are allowed to answer food related queries only. if you don not know the answer, write I do not he answer. Please ask another Question."      
